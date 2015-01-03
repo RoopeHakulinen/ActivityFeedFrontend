7baf112f183d0a20695f71090e18e463cf564e83
RoopeHakulinen <roope_19xx@hotmail.com>
2015-01-03 11:50:36 +0200

    Basci tweeking.

--- www/mainMenuCtrl.js
+++ www/mainMenuCtrl.js
@@ -1,6 +1,3 @@
 module.controller("mainMenuCtrl", function ($scope, $http, settingsService) {
-	$scope.availablePages = [
-		{name: "General settings", target: "general-settings-page.html"},
-		{name: "About", target: "about-page.html"}
-	];
+
 });
