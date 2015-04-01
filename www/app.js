7ef6ca3238a0bade2989b918c415a7c709c5bd19
RoopeHakulinen <roope_19xx@hotmail.com>
2015-04-01 23:27:56 +0300

    Trying to apply user email and token for requests for them to pass.

--- www/app.js
+++ www/app.js
@@ -5,3 +5,19 @@ var module = angular.module('app', ['onsen'], function() {
 String.prototype.splice = function( idx, rem, s ) {
 	return (this.slice(0,idx) + s + this.slice(idx + Math.abs(rem)));
 };
+
+
+module.factory('authHttpRequestInterceptor', function (userService) {
+	return {
+		request: function (config) {
+			var user = userService.getUser();
+			config.headers['X-Auth-Email'] = user["email"];
+			config.headers['X-Auth-Token'] =  user["token"];
+			return config;
+		}
+	};
+});
+
+module.config(function ($httpProvider) {
+	$httpProvider.interceptors.push('authHttpRequestInterceptor');
+});
