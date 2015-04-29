4923691d6ab4df7e3a19763e9e547d69b0f89fa1
RoopeHakulinen <roope_19xx@hotmail.com>
2015-04-29 22:07:10 +0300

    Added eventService and fixed ons-page missing from settings.

---
+++ www/eventService.js
@@ -0,0 +1,67 @@
+module.controller("eventService", function (userService) {
+
+	this.logout = function()
+	{
+		userService.logout();
+	};
+
+	document.addEventListener("backbutton", function (e){
+		var currentPage = appNavigator.getCurrentPage();
+		if (confirmationModal._element[0].style.display !== "none")
+		{
+			confirmationModal.hide();
+			e.preventDefault();
+		}
+		else if (currentPage.name.indexOf("main-view") !== -1) // If we are on survey view
+		{
+			this.logout();
+			e.preventDefault();
+		}
+		else if (currentPage.name.indexOf("login") !== -1)
+		{
+			navigator.app.exitApp();
+		}
+		else
+		{
+			console.log("Pop page.");
+		}
+	}.bind(this), false);
+
+	document.addEventListener('deviceready', function onDeviceReady() {
+		console.log("Got deviceready.");
+		window.deviceReady = true;
+
+		window.alert = function (msg)
+		{
+				navigator.notification.alert(
+					msg,
+					function ()
+					{
+					},
+					'Activer',
+					"OK"
+				);
+			}
+	}, false);
+
+	document.addEventListener('online', function() {
+		console.log("Got online event.");
+		function checkForDeviceReady()
+		{
+			if (deviceReady)
+			{
+
+			}
+			else
+			{
+				setTimeout(checkForDeviceReady.bind(this), 100)
+			}
+		}
+		checkForDeviceReady();
+	}.bind(this), false);
+
+	document.addEventListener('offline', function() {
+		console.log("Got offline event.");
+		//window.plugins.toast.showShortBottom('Connection lost');
+	}, false);
+});
