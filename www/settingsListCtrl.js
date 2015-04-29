0c957f2ee0e35f37549ecc93840c4c782e85ad1e
RoopeHakulinen <roope_19xx@hotmail.com>
2015-04-29 23:38:07 +0300

    Updated menu items.

--- www/settingsListCtrl.js
+++
@@ -1,7 +0,0 @@
-module.controller("settingsListCtrl", function ($scope, $http, $rootScope, settingsService) {
-	$scope.availablePages = [
-		{name: "General settings", target: "general-settings-page.html"},
-		{name: "About", target: "about-page.html"}
-	];
-
-});
