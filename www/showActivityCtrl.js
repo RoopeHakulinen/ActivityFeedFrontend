064206d31e0f23dedbc5a2fa1d0644e34832e027
RoopeHakulinen <roope_19xx@hotmail.com>
2015-06-04 23:23:10 +0300

    Empty the comment field after comment is sent.

--- www/showActivityCtrl.js
+++ www/showActivityCtrl.js
@@ -17,6 +17,7 @@ module.controller("showActivityCtrl", function ($scope, urlConfig, activityServi
 		activityService.sendComment($scope.activity.id, $scope.comment).then(
 			function (data)
 			{
+				$scope.comment = "";
 				$scope.activity.comments.push(data);
 			},
 			function()
